7d1070baf4fafb3dc033ff06757bd456a2941cd7
Vitalii Mudryi <vmudrij0508@gmail.com>
2024-05-09 21:47:33 +0300

    lesson 8

---
+++ lesson-8/01-jwt/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+import routes from "./routes/index.js";
+
+import "./db.js";
+
+const app = express();
+
+app.use("/api", routes);
+
+// Handle 404 Error
+app.use((req, res, next) => {
+  res.status(404).send("Not Found");
+});
+
+// Handle 500 Error
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).send("Internal Server Error");
+});
+
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
---
+++ lesson-8/01-jwt/routes/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+import authMiddleware from "../middleware/auth.js";
+
+import authRoutes from "./auth.js";
+import bookRoutes from "./books.js";
+
+const router = express.Router();
+
+router.use("/auth", authRoutes);
+router.use("/books", authMiddleware, bookRoutes);
+
+export default router;
